bf76786c3cc483dbdb2af93736281fa89ca20894
Ilya Makey <ilyamakey@gmail.com>
2017-05-16 22:24:51 +0300

    make touch response

--- build/main.js
+++ build/main.js
@@ -1,48 +1,62 @@
 var slider = document.querySelector('.price-list');
+
 slider.getCoords = function() {
   var box = this.getBoundingClientRect();
     this.left = box.left + pageXOffset;
     this.right = box.right + pageXOffset;
 };
 
-slider.addEventListener('mousedown', function(e) {
-  function moveAt(e) {
-    slider.getCoords();
-    slider.style.left = e.pageX - shiftLeft + 'px';
-  };
+function getShift(e) {
+  if(!e.pageX) {
+    return e.touches[0].pageX - slider.left;
+  } else {
+    return  e.pageX - slider.left;
+  }
+};
 
-  function removeClass(collection, className) {
-    var i;
-    for(i=0; i<collection.length; i++) {
-      collection[i].classList.remove(className);
-    }
-  };
+function moveAt(e, shift) {
+  slider.getCoords();
+  if(!e.pageX) {
+    slider.style.left = e.touches[0].pageX - shift + 'px';
+  } else {
+    slider.style.left = e.pageX - shift + 'px';
+  }
+};
 
-  function switchPagination () {
-    var pagination = document.getElementById('js-table_pagination');
-    var circle = pagination.getElementsByTagName('div');
-    var viewPort = slider.clientWidth - document.documentElement.clientWidth;
-      if (slider.left >= - (viewPort * 1/3)) {
+function switchPagination () {
+  var pagination = document.getElementById('js-table_pagination');
+  var circle = pagination.getElementsByTagName('div');
+  var viewPort = slider.clientWidth - document.documentElement.clientWidth;
+    if (slider.left >= - (viewPort * 1/3)) {
+      removeClass(circle, 'pagination__circle_active');
+      circle[0].classList.add('pagination__circle_active');
+
+    } else if (slider.left >= - (viewPort * 2/3)) {
         removeClass(circle, 'pagination__circle_active');
-        circle[0].classList.add('pagination__circle_active');
+        circle[1].classList.add('pagination__circle_active');
 
-      } else if (slider.left >= - (viewPort * 2/3)) {
-          removeClass(circle, 'pagination__circle_active');
-          circle[1].classList.add('pagination__circle_active');
+    } else {
+        removeClass(circle, 'pagination__circle_active');
+        circle[2].classList.add('pagination__circle_active');
+    }
+};
 
-      } else {
-          removeClass(circle, 'pagination__circle_active');
-          circle[2].classList.add('pagination__circle_active');
-      }
-  };
+function removeClass(collection, className) {
+  for(var i=0; i<collection.length; i++) {
+    collection[i].classList.remove(className);
+  }
+};
+
+slider.addEventListener('mousedown', function(e) {
 
   slider.getCoords();
-  var shiftLeft = e.pageX - slider.left;
   switchPagination();
+
+  var shiftLeft = getShift(e);
 
   document.onmousemove = function (e) {
     if (slider.left <= 0 && slider.right >= document.documentElement.clientWidth) {
-      moveAt(e);
+      moveAt(e, shiftLeft);
       switchPagination();
     }
   };
@@ -58,16 +72,31 @@ slider.addEventListener('mousedown', function(e) {
   };
 });
 
-//Touch events handler
-//touchstart
-// e.changedTouches[0].pageX
-// ontouchmove
-// ontouchend
-
 slider.addEventListener('touchstart', function(e) {
-  console.log('touched');
+  slider.getCoords();
+  switchPagination();
+
+  var shiftLeft = getShift(e);
+
+  document.ontouchmove = function (e) {
+    if (slider.left <= 0 && slider.right >= document.documentElement.clientWidth) {
+      moveAt(e, shiftLeft);
+      switchPagination();
+    }
+  };
+
+  document.ontouchend = function () {
+    document.ontouchmove = null;
+    slider.onmouseup = null;
+      if (slider.left >= 0) {
+        slider.style.left = 0 + 'px';
+      } else if (slider.right <= document.documentElement.clientWidth) {
+        slider.style.left = - (slider.clientWidth - document.documentElement.clientWidth) + 'px';
+      }
+  }
 });
 
+//browser default moving handler reset
 slider.ondragstart = function() {
   return false;
 };
